5831faca9baa9b4f92e2a2c8156532b7be8ceeeb
like <20783506632@qq.com>
2017-09-20 21:15:21 +0800

    add dropdown function

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/alert
+//= require bootstrap/dropdown
 //= require bootstrap/collapse
 //= require bootstrap/modal
 //= require_tree .
