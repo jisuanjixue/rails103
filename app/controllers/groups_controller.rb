9e9c536ecb0391e58f9c0053b4eafab4c744b482
like <20783506632@qq.com>
2017-09-29 20:56:50 +0800

    people can't see button unless he is group onwer

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
 	def index
 		@groups = Group.all
 	end
@@ -9,7 +10,6 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def show
@@ -27,7 +27,6 @@ class GroupsController < ApplicationController
     end
 
     def update
-      @group = Group.find(params[:id])
       if @group.update(group_params)
         redirect_to groups_path
       else
@@ -36,7 +35,6 @@ class GroupsController < ApplicationController
     end
 
     def destroy
-      @group = Group.find(params[:id])
       @group.destroy
       redirect_to groups_path
       flash[:alert] = "你已经删除!"
@@ -44,6 +42,10 @@ class GroupsController < ApplicationController
 
 
     private
+
+    def find_group_and_check_permission
+      @group = Group.find(params[:id])
+    end
 
   def group_params
     params.require(:group).permit(:title, :description)
