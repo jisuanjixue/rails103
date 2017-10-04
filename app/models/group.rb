d2c6d844830e10297736674e1450408a3d1a8d89
like <20783506632@qq.com>
2017-10-04 14:06:34 +0800

    implement group relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,6 @@ class Group < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :posts
+  has_many :GroupRelationships
+  has_many :members, through: :GroupRelationships, source: :user
 end
