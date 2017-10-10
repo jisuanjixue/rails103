ec941d22539d42ca08eb7be5f217d80f7b911257
like <20783506632@qq.com>
2017-10-10 16:51:42 +0800

    when created a group, join it automatically

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :installs
   resources :groups do
+     member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
   root 'groups#index'
